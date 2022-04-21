603c249c89cc3ef09c60517a0b076f4ecb1cc746
CARLOS IVAN RAMIREZ RENDON <cramirezr@totalplay.corp>
2022-04-21 17:14:31 -0500

    Clase 19

---
+++ src/main/java/com/platzi/posgresql/persistence/ProductRepository.java
@@ -0,0 +1,24 @@
+package com.platzi.posgresql.persistence;
+
+import com.platzi.posgresql.persistence.crud.ProductoCrudRepository;
+import com.platzi.posgresql.persistence.entity.Producto;
+
+import java.util.List;
+import java.util.Optional;
+
+public class ProductRepository {
+    private ProductoCrudRepository productoCrudRepository;
+
+    public List<Producto> getAll(){
+        return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoCrudRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+}
---
+++ src/main/java/com/platzi/posgresql/persistence/entity/ComprasProducto.java
@@ -0,0 +1,28 @@
+package com.platzi.posgresql.persistence.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "compras_productos")
+public class ComprasProducto {
+
+    @EmbeddedId
+    private ComprasProductoPK id;
+
+    private Integer cantidad;
+    private Double total;
+    private Boolean estado;
+
+    @ManyToOne
+    @JoinColumn(name = "id_compra",insertable = false,updatable = false)
+    private Compra compra;
+
+    @ManyToOne
+    @JoinColumn(name = "id_producto",insertable = false,updatable = false)
+    private Producto producto;
+
+
+}
