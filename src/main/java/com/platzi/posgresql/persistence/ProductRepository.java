a0976daae1fb8abedfe054d753e3eb1fb83d06a3
CARLOS IVAN RAMIREZ RENDON <cramirezr@totalplay.corp>
2022-04-21 17:26:49 -0500

    Final clase 20

--- src/main/java/com/platzi/posgresql/persistence/ProductRepository.java
+++ src/main/java/com/platzi/posgresql/persistence/ProductRepository.java
@@ -2,10 +2,13 @@ package com.platzi.posgresql.persistence;
 
 import com.platzi.posgresql.persistence.crud.ProductoCrudRepository;
 import com.platzi.posgresql.persistence.entity.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+//Decorar la clase por buena practica se puede usar @Repository o @Componen (esta es mas generica)
+@Repository
 public class ProductRepository {
     private ProductoCrudRepository productoCrudRepository;
 
@@ -19,6 +22,18 @@ public class ProductRepository {
 
     public Optional<List<Producto>> getEscasos(int cantidad){
         return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCrudRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCrudRepository.deleteById(idProducto);
     }
 
 }
